439e225032c4cf177dd607bcd28cdb91579f3aa9
Rohan patel <oneplusrohan@hotmail.com>
2019-04-20 22:46:59 -0400

    Private Data Member used of Constructor in Inheritance

--- InterviewBasicHandsOn/OOP/Inheritance/HirarichalInheritance.java
+++ InterviewBasicHandsOn/OOP/Inheritance/HirarichalInheritance.java
@@ -4,41 +4,127 @@ public class HirarichalInheritance {
 
 	public static void main(String[] args) 
 	{
-		Son obj = new Son();
-		obj.disp();
+		Son son = new Son("Rohan", "Dilipbhai", 25);
+		System.out.println("My Name : " + son.getMyName());		
+		System.out.println("Father Name : " + son.getFatherName());
+		System.out.println("My Age : " + son.getAge());
 		
-		Daughter obj2 = new Daughter();
-		obj2.disp();
+		Daughter daughter = new Daughter("DRT", "Dilipbhai");
+		System.out.println("My Name : " + daughter.getDaughterName());		
+		System.out.println("Father Name : " + daughter.getFatherName());
 	}
 
 }
-
-class Father
+class Father  //Super Class
 {
-	String fName ="Dilip";
-	void disp()
+	private String fName; //Private DataType
+	
+	public Father(String fName) //Constructor
 	{
-		System.out.println("I'm Father");
+		this.fName = fName;
+	}
+	public String getFatherName()
+	{
+		return fName;
+		
 	}
 }
 
 class Son extends Father
 {
-	String myName ="Rohan";
-	
-	void disp()
+	private String myName; //Private DataType
+	private int age;
+	public Son(String myName, String fName, int age) //Constructor
 	{
-		System.out.println("My Name " + myName);
-		System.out.println("My father Name is "+ fName);
+		super(fName); //Calling SuperClass Constructor
+		this.myName = myName;
+		this.age = age;
+	}
+	
+	public String getMyName()
+	{
+		return myName;
+		
+	}
+	public int getAge()
+	{
+		return age;
+		
 	}
 }
 
 class Daughter extends Father
 {
-	String myName ="DRT";
-	void disp()
+	private String daughterName; //Private DataType
+	
+	public Daughter(String daughterName, String fName) //Constructor
 	{
-		System.out.println("My Name " + myName);
-		System.out.println("My father Name is "+ fName);
+		super(fName); //Calling SuperClass Constructor
+		this.daughterName = daughterName;
+	}
+	
+	public String getDaughterName()
+	{
+		return daughterName;
+		
 	}
 }
+
+//Using Getter and setter Method
+/*public class HirarichalInheritance {
+
+	public static void main(String[] args) 
+	{
+		Son son = new Son();
+		son.setfName("Dilipbhai");
+		son.setMyName("Rohan");
+		
+		System.out.println("My Name: " + son.getMyName() + " \nfather Name: " + son.getfName());
+		
+	}
+
+}
+class Father
+{
+	private String fName;
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	
+	
+}
+
+class Son extends Father
+{
+	private String myName;
+
+	public String getMyName() {
+		return myName;
+	}
+
+	public void setMyName(String myName) {
+		this.myName = myName;
+	}
+	
+	
+}
+
+class Daughter extends Father
+{
+	private String myName;
+
+	public String getMyName() {
+		return myName;
+	}
+
+	public void setMyName(String myName) {
+		this.myName = myName;
+	}
+	
+	
+}*/
--- InterviewBasicHandsOn/OOP/Inheritance/SingleInheritence.java
+++ InterviewBasicHandsOn/OOP/Inheritance/SingleInheritence.java
@@ -1,35 +1,27 @@
 package Inheritance;
 
-public class SingleInheritence
-{
-	public static void main(String[] args) 
-	{
+public class SingleInheritence {
+	public static void main(String[] args) {
 		Child c = new Child();
 		c.disp();
-	
-		
+
 	}
-	
+
 }
 
-class Parent
-{
-	int x=10;
-	void disp()
-	{
+class Parent {
+	int x = 10;
+
+	void disp() {
 		System.out.println("Parent");
 	}
 }
 
-class Child extends Parent
-{
-	int y = x +10;
-	void disp()
-	{
-		System.out.println("Child: "+y);
+class Child extends Parent {
+	int y = x + 10;
+
+	void disp() {
+		System.out.println("Child: " + y);
 	}
-	
+
 }
-	
-
-
