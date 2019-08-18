e513ee65244a92f23901bfc8a36f5d0721580f3c
Rohan patel <oneplusrohan@hotmail.com>
2019-08-17 22:40:26 -0400

    Given a string, remove the consecutive character pairs repetitively

---
+++ GS[JavaFundamentals]/JavaFundamentals/EasyCodingInterview/RemoveConsecutiveRepeatedChar.java
@@ -0,0 +1,29 @@
+package EasyCodingInterview;
+
+/*
+ * Logic:
+ * 
+ * Check 2nd character with 1st character 
+ * if 2ndcharacter != 1st character then add it to Output String
+ * set output string with 1st character.
+ */
+public class RemoveConsecutiveRepeatedChar {
+
+	public static void main(String[] args) 
+	{
+		String str = "aaaabbbcccbbbaaa";
+		String output = str.valueOf(str.charAt(0));
+		
+		for(int i=1; i<str.length(); i++)
+		{
+			if(str.charAt(i) != str.charAt(i-1))
+			{
+				output += str.charAt(i);
+			}
+		}
+		
+		System.out.println(output);	
+
+	}
+
+}
