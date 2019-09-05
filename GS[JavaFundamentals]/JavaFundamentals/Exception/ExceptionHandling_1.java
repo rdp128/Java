d1cb04d4eaa8bef361eeae661322ab7f996d9d1c
Rohan patel <oneplusrohan@hotmail.com>
2019-09-05 15:10:32 -0400

    Exception Re-Visited 2.0

--- GS[JavaFundamentals]/JavaFundamentals/Exception/ExceptionHandling_1.java
+++ GS[JavaFundamentals]/JavaFundamentals/Exception/ExceptionHandling_1.java
@@ -1,16 +1,28 @@
 package Exception;
 
+/* 
+ * We have two option to handle User define exception
+ * 1)Handled in Method where exception occur
+ *   - in this case we handle it in  AmountAdder method
+ * 2)Handled where we calling method that has exception
+ *   - in this case we handle it in  ExceptionHandling_1 class where we calling AmountAdder Method
+ * 3)Also if we don't want to handle we can throw it to JVM add throws CurrencyNotMatchException in both AmountAdder and  ExceptionHandling_1 
+ *   But this is not good practice as we create user define exception to handle it ONLY.
+ */
 public class ExceptionHandling_1 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
 		Amount obj1 = new Amount("Dollar", 10);
 		Amount obj2 = new Amount("Dollar", 20);
 		
-		System.out.println(AmountAdder.addAmounts(obj1, obj2));
-		
+		try {
+			AmountAdder.addAmounts(obj1, obj2);
+		} catch (CurrencyNotMatchException e) 
+		  {
+			System.out.println(e);
+		  }		
 	}
-
 }
 
 class CurrencyNotMatchException extends Exception
@@ -18,8 +30,7 @@ class CurrencyNotMatchException extends Exception
 	public CurrencyNotMatchException() 
 	{
 		super("Currency Not Matched");
-	}
-	
+	}	
 }
 class Amount
 {
@@ -35,21 +46,17 @@ class Amount
 
 class AmountAdder
 {
-	static Amount addAmounts(Amount amount1, Amount amount2) 
+	public static void addAmounts(Amount amount1, Amount amount2) throws CurrencyNotMatchException
 	{
 		if( !amount1.currency.equals(amount2.currency))
 		{
-			try 
-			{
+			
 				throw new CurrencyNotMatchException();
-			} catch (CurrencyNotMatchException e) 
-			{
-				System.out.println(e);
-			}
 		}
-		
-		return new Amount("HI" , (amount1.amount + amount2.amount));
-		
-	}
-	
+		else
+		{
+			System.out.print("Currency Match Total: ");
+			System.out.println(amount1.amount + amount2.amount);
+		}
+	}	
 }
--- GS[JavaFundamentals]/JavaFundamentals/Exception/UerDefineCheckedException.java
+++ GS[JavaFundamentals]/JavaFundamentals/Exception/UerDefineCheckedException.java
@@ -6,15 +6,49 @@ public class UerDefineCheckedException {
 
 	public static void main(String[] args) 
 	{
-		Scanner sc = new Scanner (System.in);
+		CreateException obj = new CreateException();
 		
-		int money = 10000;
-		System.out.println("Enter Withdraw amount: ");
-		int withdraw = sc.nextInt();
-		int remain = money - withdraw;
-		System.out.println("Rmaining :" + remain);
+		obj.atm(2000);
 		
+	}
+}
 
+class LowBalanceException extends Exception
+{
+	public LowBalanceException() {
+		super("Insufficient Balance");
 	}
 
+}
+
+class CreateException
+{
+	
+	public void atm(int n) 
+	{
+		Scanner sc = new Scanner (System.in);
+		
+		int money = n;
+		
+		System.out.println("Enter withdraw amount: ");
+		int withdraw = sc.nextInt();
+		
+		int remain = money - withdraw;
+		
+		if(remain < money)
+		{
+			try {
+				throw new LowBalanceException();
+			} catch (LowBalanceException e) {
+				System.out.println(e);
+			}
+		}
+		else
+		{
+			System.out.println("Rmaining Blance :" + remain);
+		}
+		sc.close();
+	}
+	
+	
 }
