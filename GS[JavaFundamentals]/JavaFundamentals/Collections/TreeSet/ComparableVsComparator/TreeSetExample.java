4464e38f92f1c29d07565fac43ce4b00c777c6ae
Rohan patel <oneplusrohan@hotmail.com>
2019-09-14 14:55:51 -0400

    TreeSet Comparable Vs Comparator

---
+++ GS[JavaFundamentals]/JavaFundamentals/Collections/TreeSet/ComparableVsComparator/TreeSetExample.java
@@ -0,0 +1,17 @@
+package Collections.TreeSet.ComparableVsComparator;
+
+import java.util.TreeSet;
+
+public class TreeSetExample 
+{
+	public static void main(String[] args)
+	{
+		TreeSet<String> ts = new TreeSet<String>();
+		
+		ts.add("Ab");
+		ts.add("pq");
+		
+		System.out.println(ts);
+	}
+	
+}
